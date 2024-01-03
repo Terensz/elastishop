3ebc597fbc4cb5975d11d9b11187d1b9af4df5b1
Papp Ferenc <terencecleric@gmail.com>
2024-01-03 01:36:37 +0100

    +

--- elastisite/elastisite_source/framework/packages/WebshopPackage/dataProvider/PackDataProvider.php
+++ elastisite/elastisite_source/framework/packages/WebshopPackage/dataProvider/PackDataProvider.php
@@ -162,7 +162,8 @@ class PackDataProvider extends Service
                 $packItemIsActive = false;
                 // dump($packItemData);exit;
 
-
+dump($packItem->getProduct()->getStatus());
+dump(Product::STATUS_ACTIVE);
 dump($packItem->getProduct()->getStatus() !== Product::STATUS_ACTIVE);
 dump(!$packItemData['product']['actualPrice']);
 dump(!$packItemData['product']['activePrice']);
