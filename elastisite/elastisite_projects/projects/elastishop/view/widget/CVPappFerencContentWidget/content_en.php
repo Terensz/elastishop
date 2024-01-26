6d9a77895288e650bbc81e7f2ed2cbb788d24b43
Papp Ferenc <terencecleric@gmail.com>
2024-01-26 13:23:30 +0100

    +

---
+++ elastisite/elastisite_projects/projects/elastishop/view/widget/CVPappFerencContentWidget/content_en.php
@@ -0,0 +1,134 @@
+        <div class="row">
+            <div class="col-xs-12 col-sm-7">
+                <!-- <div class="card">
+                    <div class="card-footer">
+                        <span class="title-card">Papp Ferenc, Webfejlesztő</span>
+                    </div>
+                </div> -->
+                <div class="block-title">Ferenc Papp, web developer</div>
+
+                <div class="card">
+                    <div class="card-header">
+                        <b>Quick info</b>
+                    </div>
+                    <div class="card-footer">
+                        PHP, Symfony, Laravel, Yii
+                    </div>
+                    <div class="card-footer">
+                        Javascript, jQuery
+                    </div>
+                    <div class="card-footer">
+                        SEO, cookie handling, GDPR, Git, SVN
+                    </div>
+                    <div class="card-footer">
+                        Linux server management, MacOS development environment
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-header">
+                        <b>Contact data</b>
+                    </div>
+                    <div class="card-footer">
+                        <b>Mobile:</b> +36705150551
+                    </div>
+                    <div class="card-footer">
+                        <b>E-mail:</b> terencecleric@gmail.com
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-5">
+                <div class="card">
+                    <img class="webshopProductImage webshopThumbnail" src="/image/pf_image.jpeg" style="width: 100%; height: auto;">
+                </div>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-header">
+                <b>Web development abilities</b>
+            </div>
+            <div class="card-footer">
+                I'm learning web development since 2004, I know many old and many new technologies.
+            </div>
+            <div class="card-footer">
+                I've already worked with Laravel, Yii and Symfony, so I can understand any further OOP frameworks in few hours.
+            </div>
+            <div class="card-footer">
+                I've made many Javascript / jQuery helpers
+            </div>
+            <div class="card-footer">
+                I'm using frequently Laravel Livewire
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-header">
+                <b>Other IT abilities</b>
+            </div>
+            <div class="card-footer">
+                I have the knowledge of the GDPR, cookie handling and SEO.
+            </div>
+            <div class="card-footer">
+                I was a development supporter for several years, I can handle long and complex SQL queries.
+            </div>
+            <div class="card-footer">
+                I have medium level knowledge of managing Linux servers.
+            </div>
+            <div class="card-footer">
+                I'm a beginner developer of the Swift language, I spend several months to learn how to make iOS applications.
+            </div>
+            <div class="card-footer">
+                I'm using Git frequently, also I know SVN.
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-header">
+                <b>General abilities</b>
+            </div>
+            <div class="card-footer">
+                I have exceptional spelling and wording in Hungarian.
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-header">
+                <b>Achievements</b>
+            </div>
+            <div class="card-footer">
+                I have made an own framework in 5 years.
+            </div>
+            <div class="card-footer">
+                I have made a webshop engine which is 100% GDPR compatible.
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-header">
+                <b>Language knowledge</b>
+            </div>
+            <div class="card-footer">
+                <b>English</b>: I communicate fluently in speech and writing. I read frequently in English, I understand the terminologies of law and economics, astronomy and phisics.
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-header">
+                <b>Relevant IT exoeriences</b>
+            </div>
+            <div class="card-footer">
+            <b>2013 - 2017: TMX LTD.</b> - I was a development supporter for 2 years.<br>
+            I was responsible for writing all the hotfix queries to fix database errors caused by software bugs and creating complex summary statements.<br>
+            I helped to developers to write them the data migration scrips.
+            </div>
+            <div class="card-footer">
+            <b>2017 - 2021: Association for the Legal Protection of Performers (EJI)</b> - At first I was a junior Symfony developer. While spending 3.5 years here I learnt object oriented programming and working with frameworks. My techer was Attila Krucsó senior developer.
+            </div>
+            <div class="card-footer">
+            <b>2018 - 2023: <span style="font: 18px Neuropol-Regular; color: #000;">Elasti</span><span style="font: 18px Neuropol-Regular; color: #337389;">Site</span> project (my own framework)</b> - 
+                When I started the development, the frameworks did not support dynamic content change.<br>
+                Or: the ones did (e.g. early versions of React and Angular), were not able to achieve good placement in the Google search results. I solved this problem with my own framework.<br>
+                Meanwhile the modern versions of frameworks also solved this (e.g. Laravel Livewire), therefore I discontinue ElastiSite framework in this form. 
+            </div>
+        </div>
